b9e6d2c2e87a6d71cfeb1a988bc8f41e13835976
kzsolt1984 <kzsolt1984@gmail.com>
2020-12-03 17:58:39 +0100

    finish

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# turnpasswordgeneratorcli
+# Turn password generator cli
+Creating encrypted TURN credentials
+
+## Installation Steps (if applicable)
+
+1. Clone this repo
+2. Run `npm install`
+3. Install the module globally with `npm install -g` from within the project directory
+4. Run `generate`
+
+## License
+The MIT License (MIT)
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+#!/usr/bin/env node
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-
+const inquirer  = require('./lib/inquirer');
+const generator  = require('./lib/generator');
 
 clear();
 
 console.log(
   chalk.yellow(
-    figlet.textSync('Ginit', { horizontalLayout: 'full' })
+    figlet.textSync('Generator', { horizontalLayout: 'full' })
   )
 );
+
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials();
+  const generated = await generator(credentials.secret, credentials.validityInSec, credentials.name)
+
+  console.log(`
+    Username: ${chalk.blue(generated.username)}
+    Password: ${chalk.green(generated.password)}
+  `)
+};
+
+run();
